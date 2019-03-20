7424d6c568cca5b34119285f177084f928368510
ThalKod <tmarcelin@thal.tech>
2019-03-20 12:40:08 -0400

    add Video model

---
+++ src/models/VIdeo.ts
@@ -0,0 +1,51 @@
+import { Schema, Document, model, Model } from "mongoose";
+
+export interface IVideo extends Document{
+    name: string,
+    handle: string,
+    mimeType: string,
+    url: string,
+    duration: number,
+    size: number,
+    author: Schema.Types.ObjectId,
+    defaultCoverPhoto: string,
+    description: string,
+    tags: { id: string, text: string}[],
+    viewCount: number
+    createdAt?: number,
+    updatedAt?: number
+}
+
+const videosSchema = new Schema({
+    name: {
+        type: String,
+        unique: true,
+        require: true
+    },
+    handle: {
+        type: String,
+        unique: true,
+        require: true
+    },
+    mimeType: String,
+    url: {
+        type: String,
+        unique: true,
+        require: true,
+    },
+    duration: Number,
+    size: Number,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    defaultCoverPhoto: String,
+    description: String,
+    tags: [],
+    viewCount: {
+        type: Number,
+        default: 0
+    },
+}, {timestamps: true});
+
+export const Video:Model<IVideo> =  model<IVideo>("Video", videosSchema);
